d6d2504782293b7c04a5e3730a5db543d1990b9b
dustinrosebery <dustinrosebery>
2016-02-16 21:55:06 -0700

    Initial Commit of Calculator and CalculatorTest

---
+++ CSE360_Assignment3/src/cse360assign3/Calculator.java
@@ -0,0 +1,68 @@
+/**
+ * @author Dustin Rosebery
+ * PIN: 721
+ * 
+ * This class performs simple arithmetic functions and is meant as an introduction to using a repository
+ */
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * the constructor creates a calculator object
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal is an accessory method to retrieve the value of the various calculations
+	 * @return the value of the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add adds an input value to the total
+	 * @param value to be added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract subtracts an input value from the total
+	 * @param value to be subtracted from the total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiply multiplies the value by the total
+	 * @param value to be multiplied with total
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide divides an input value from the total
+	 * @param value to be be divided from the total
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory returns a string of the previous actions performed on the total
+	 * @return a string of previous commands
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
